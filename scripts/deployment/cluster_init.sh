8d0fe5dfcc7e85a48e0e8a17653b7f587de5731a
Oguz Pastirmaci <oguzp@outlook.com>
2020-05-28 08:36:01 -0700

    Update cluster_init.sh

--- scripts/deployment/cluster_init.sh
+++ scripts/deployment/cluster_init.sh
@@ -34,15 +34,37 @@ INSTANCE_POOL_STATE=$($OCI_CLI_LOCATION compute-management instance-pool get --i
 #    INSTANCE_POOL_STATE=$($OCI_CLI_LOCATION compute-management instance-pool get --instance-pool-id $INSTANCE_POOL_ID | jq -r '.data."lifecycle-state"')
 #done
 
-INSTANCES_TO_ADD=$($OCI_CLI_LOCATION compute-management instance-pool list-instances --instance-pool-id $INSTANCE_POOL_ID --region $REGION --compartment-id $COMPARTMENT_ID | jq -r '.data[]."id"')
-echo $INSTANCES_TO_ADD
-    
 #echo "$(date) Changing all.q's tmpdir to /nvme/tmp"
 #qconf -rattr queue tmpdir /nvme/tmp all.q
 qconf -Ap $SGE_ROOT/simcores_pe
 qconf -aattr queue pe_list simcores all.q
 
-/home/sgeadmin/ocisge/$CLUSTER_POSTFIX/scripts/add_exec_host.sh 0
+INSTANCES_TO_ADD=$($OCI_CLI_LOCATION compute-management instance-pool list-instances --instance-pool-id $INSTANCE_POOL_ID --region $REGION --compartment-id $COMPARTMENT_ID | jq -r '.data[]."id"')
+    until [ $($OCI_CLI_LOCATION compute-management instance-pool get --instance-pool-id $INSTANCE_POOL_ID | jq -r '.data."lifecycle-state"') == "RUNNING" ]; do
+    echo "$(date) Waiting for Instance Pool state to be RUNNING"
+    sleep 15
+    done
+    MASTER_PRIVATE_IP=$(curl -s http://169.254.169.254/opc/v1/vnics/ | jq -r '.[].privateIp')
+    MASTER_HOSTNAME=$(hostname)
+    for INSTANCE in $INSTANCES_TO_ADD; do
+        PRIVATE_IP=$($OCI_CLI_LOCATION compute instance list-vnics --instance-id $INSTANCE --compartment-id $COMPARTMENT_ID | jq -r '.data[]."private-ip"')
+        echo $PRIVATE_IP
+        COMPUTE_HOSTNAME_TO_ADD=$($OCI_CLI_LOCATION compute instance get --instance-id $INSTANCE | jq -r '.data."display-name"')
+        echo $COMPUTE_HOSTNAME_TO_ADD
+        echo $PRIVATE_IP $COMPUTE_HOSTNAME_TO_ADD | sudo tee -a /etc/hosts
+            until ssh -q -oStrictHostKeyChecking=no $SGE_ADMIN@$COMPUTE_HOSTNAME_TO_ADD exit; do
+                echo "$(date) Waiting for remote exec host to respond"
+                sleep 10
+            done
+        ssh sgeadmin@$COMPUTE_HOSTNAME_TO_ADD "sudo hostname $COMPUTE_HOSTNAME_TO_ADD"
+        echo $PRIVATE_IP $COMPUTE_HOSTNAME_TO_ADD | ssh sgeadmin@$COMPUTE_HOSTNAME_TO_ADD "sudo sh -c 'cat >>/etc/hosts'"
+        echo $MASTER_PRIVATE_IP $MASTER_HOSTNAME sge-master | ssh sgeadmin@$COMPUTE_HOSTNAME_TO_ADD "sudo sh -c 'cat >>/etc/hosts'"
+        sed -i 's/^EXEC_HOST_LIST=.*/EXEC_HOST_LIST="'"$COMPUTE_HOSTNAME_TO_ADD"'"/' $CONFIG_FILE
+        scp $CONFIG_FILE $COMPUTE_HOSTNAME_TO_ADD:$CONFIG_FILE
+        cd $SGE_ROOT && ./inst_sge -x -auto $CONFIG_FILE
+        sleep 10
+        ssh sgeadmin@$COMPUTE_HOSTNAME_TO_ADD "sudo $SGE_ROOT/$CELL_NAME/common/sgeexecd stop && sudo $SGE_ROOT/$CELL_NAME/common/sgeexecd start"
+    done
 
 echo "$(date) Cluster initialization completed"
 

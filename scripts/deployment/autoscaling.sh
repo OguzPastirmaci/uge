4029ca02fb66b8dff0c6e796352e0ce4cdea44f5
Oguz Pastirmaci <oguzp@outlook.com>
2020-05-28 12:07:18 -0700

    Update autoscaling.sh

--- scripts/deployment/autoscaling.sh
+++ scripts/deployment/autoscaling.sh
@@ -5,7 +5,6 @@ set -x
 . /home/sgeadmin/ocisge/scripts/info.sh
 . $SGE_ROOT/$CELL_NAME/common/settings.sh
 
-export CLUSTER_SCALING_FREQUENCY=300
 export CURRENT_INSTANCE_POOL_SIZE=$($OCI_CLI_LOCATION compute-management instance-pool get --instance-pool-id $INSTANCE_POOL_ID --region $REGION --query 'data.size' --raw-output)
 
 NUMBER_OF_CORES_PER_SLAVE=$(echo "${SLAVE_SHAPE##*.}")
@@ -15,7 +14,7 @@ RUNNING_JOBS=$(qstat -u '*' | awk ' { if ($5 == "r") print $0 }' | wc -l)
 PENDING_JOBS=$(qstat -u '*' | awk ' { if ($5 == "qw" || $5 == "hqw")  print $0 }' | wc -l)
 
 CURRENT_UTILIZATION=$(echo "scale=2; 100 / $NUMBER_OF_TOTAL_CORES * $NUMBER_OF_USED_CORES" | bc -l)
-DESIRED_UTILIZATION=50
+DESIRED_UTILIZATION=10
 
 echo -e "\n$(date) -- Checking the cluster for autoscaling"
 echo "$(date) -- Number of running jobs in the cluster: $RUNNING_JOBS"

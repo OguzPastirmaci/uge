c8ed923456ae0640752db789afce28f3517e622f
Oguz Pastirmaci <oguzp@outlook.com>
2020-05-28 08:30:49 -0700

    Update cluster_init.sh

--- scripts/deployment/cluster_init.sh
+++ scripts/deployment/cluster_init.sh
@@ -36,29 +36,14 @@ INSTANCE_POOL_STATE=$($OCI_CLI_LOCATION compute-management instance-pool get --i
 
 INSTANCES_TO_ADD=$($OCI_CLI_LOCATION compute-management instance-pool list-instances --instance-pool-id $INSTANCE_POOL_ID --region $REGION --compartment-id $COMPARTMENT_ID | jq -r '.data[]."id"')
 echo $INSTANCES_TO_ADD
-
-for INSTANCE in $INSTANCES_TO_ADD; do
-    PRIVATE_IP=$($OCI_CLI_LOCATION compute instance list-vnics --instance-id $INSTANCE | jq -r '.data[]."private-ip"')
-    echo $PRIVATE_IP
-    COMPUTE_HOSTNAME_TO_ADD=$($OCI_CLI_LOCATION compute instance get --instance-id $INSTANCE | jq -r '.data."display-name"')
-    echo $COMPUTE_HOSTNAME_TO_ADD
-    echo $PRIVATE_IP $COMPUTE_HOSTNAME_TO_ADD | sudo tee -a /etc/hosts
-    until ssh -q -oStrictHostKeyChecking=no $SGE_ADMIN@$COMPUTE_HOSTNAME_TO_ADD exit; do
-        echo "$(date) Waiting for remote exec host to respond"
-        sleep 10
-    done 
-    ssh sgeadmin@$COMPUTE_HOSTNAME_TO_ADD "sudo hostname $COMPUTE_HOSTNAME_TO_ADD"
-    echo $PRIVATE_IP $COMPUTE_HOSTNAME_TO_ADD | ssh sgeadmin@$COMPUTE_HOSTNAME_TO_ADD "sudo sh -c 'cat >>/etc/hosts'"
-    echo $MASTER_PRIVATE_IP $MASTER_HOSTNAME | ssh sgeadmin@$COMPUTE_HOSTNAME_TO_ADD "sudo sh -c 'cat >>/etc/hosts'"
-    sed -i 's/^EXEC_HOST_LIST=.*/EXEC_HOST_LIST="'"$COMPUTE_HOSTNAME_TO_ADD"'"/' $CONFIG_FILE
-    scp $CONFIG_FILE $COMPUTE_HOSTNAME_TO_ADD:$CONFIG_FILE
-    cd $SGE_ROOT && ./inst_sge -x -auto $CONFIG_FILE
-    sleep 10
-    ssh sgeadmin@$COMPUTE_HOSTNAME_TO_ADD "sudo $SGE_ROOT/$CELL_NAME/common/sgeexecd stop && sudo /tools/gridengine/uge/sge-master-default/common/sgeexecd start"
-done
     
 #echo "$(date) Changing all.q's tmpdir to /nvme/tmp"
 #qconf -rattr queue tmpdir /nvme/tmp all.q
 qconf -Ap $SGE_ROOT/simcores_pe
 qconf -aattr queue pe_list simcores all.q
+
+/home/sgeadmin/ocisge/${cluster_postfix}/scripts/add_exec_host.sh 0
+
 echo "$(date) Cluster initialization completed"
+
+

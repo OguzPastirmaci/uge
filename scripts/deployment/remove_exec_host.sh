4c22cb62d3b8aeac6659a8b6a085c999dd5d78f9
Oguz Pastirmaci <oguzp@outlook.com>
2020-05-28 11:12:12 -0700

    Update remove_exec_host.sh

--- scripts/deployment/remove_exec_host.sh
+++ scripts/deployment/remove_exec_host.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 
 . /home/sgeadmin/ocisge/$CLUSTER_POSTFIX/scripts/info.sh
 
@@ -8,12 +8,12 @@ NEW_INSTANCE_POOL_SIZE=$((CURRENT_INSTANCE_POOL_SIZE - 1))
 
 if [ "$NEW_INSTANCE_POOL_SIZE" -lt "$CLUSTER_MIN_SIZE" ]
 then
-	echo "$(date) Cluster already has the minimum number of $CLUSTER_MIN_SIZE nodes"
+	echo "$(date) -- Cluster already has the minimum number of $CLUSTER_MIN_SIZE nodes"
 elif [ $(expr $(date +%s) - $(stat $SCALING_LOG -c %Y)) -le "$SCALING_COOLDOWN_IN_SECONDS" ]
 then
-	echo "$(date) Last scaling operation happened in the last $SCALING_COOLDOWN_IN_SECONDS seconds, skipping"
+	echo "$(date) -- Last scaling operation happened in the last $SCALING_COOLDOWN_IN_SECONDS seconds, skipping"
 else
-	echo "$(date) Starting to scale in the cluster from $CURRENT_INSTANCE_POOL_SIZE nodes to $NEW_INSTANCE_POOL_SIZE nodes"
+	echo "$(date) -- Starting to scale in the cluster from $CURRENT_INSTANCE_POOL_SIZE nodes to $NEW_INSTANCE_POOL_SIZE nodes"
     INSTANCE_TO_DELETE=$($OCI_CLI_LOCATION compute-management instance-pool list-instances --instance-pool-id $INSTANCE_POOL_ID --region $REGION --compartment-id $COMPARTMENT_ID --sort-by TIMECREATED --sort-order DESC | jq -r '.data[] | select(.state=="Running") | .id')
     echo $INSTANCE_TO_DELETE
     PRIVATE_IP=$($OCI_CLI_LOCATION compute instance list-vnics --instance-id $INSTANCE_TO_DELETE --compartment-id $COMPARTMENT_ID | jq -r '.data[]."private-ip"')
@@ -26,5 +26,5 @@ else
     sudo sed -i".bak" "/$COMPUTE_HOSTNAME_TO_REMOVE/d" /etc/hosts
     $OCI_CLI_LOCATION compute instance terminate --region $REGION --instance-id $INSTANCE_TO_DELETE --force
     $OCI_CLI_LOCATION compute-management instance-pool update --instance-pool-id $INSTANCE_POOL_ID --size $NEW_INSTANCE_POOL_SIZE
-    echo "$(date) Scaled in the cluster from $CURRENT_INSTANCE_POOL_SIZE nodes to $NEW_INSTANCE_POOL_SIZE nodes" >> $SCALING_LOG
+    echo "$(date) -- Scaled in the cluster from $CURRENT_INSTANCE_POOL_SIZE nodes to $NEW_INSTANCE_POOL_SIZE nodes" >> $SCALING_LOG
 fi

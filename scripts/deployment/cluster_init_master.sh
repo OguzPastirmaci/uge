783320aaa8601600f5099c816e3f689ab610bf46
Oguz Pastirmaci <oguzp@outlook.com>
2020-06-01 16:21:17 -0700

    Update cluster_init_master.sh

--- scripts/deployment/cluster_init_master.sh
+++ scripts/deployment/cluster_init_master.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-echo "Cluster postfix: $CLUSTER_POSTFIX"
+echo "Cluster postfix: <clusterpostfix>"
 
-. /home/sgeadmin/ocisge/$CLUSTER_POSTFIX/scripts/info.sh
+. /home/sgeadmin/ocisge/<clusterpostfix>/scripts/info.sh
 
 echo "$(date) Starting cluster initialization"
 # Add ADMIN and SUBMIT host
@@ -12,9 +12,9 @@ MASTER_PRIVATE_IP=$(curl -s http://169.254.169.254/opc/v1/vnics/ | jq -r '.[].pr
 MASTER_HOSTNAME=$(hostname)
 echo $MASTER_PRIVATE_IP $MASTER_HOSTNAME | tee -a /etc/hosts
 
-cd /home/sgeadmin/ocisge/$CLUSTER_POSTFIX/scripts
+cd /home/sgeadmin/ocisge/<clusterpostfix>/scripts
 wget https://raw.githubusercontent.com/OguzPastirmaci/misc/master/uge.conf
-cp /home/sgeadmin/ocisge/$CLUSTER_POSTFIX/scripts/uge.conf $CONFIG_FILE
+cp /home/sgeadmin/ocisge/<clusterpostfix>/scripts/uge.conf $CONFIG_FILE
 sed -i 's/^ADMIN_HOST_LIST=.*/ADMIN_HOST_LIST="'"$MASTER_HOSTNAME"'"/' $CONFIG_FILE
 sed -i 's/^SUBMIT_HOST_LIST=.*/SUBMIT_HOST_LIST="'"$MASTER_HOSTNAME"'"/' $CONFIG_FILE
 sed -i 's/^CELL_NAME=.*/CELL_NAME="'"$CELL_NAME"'"/' $CONFIG_FILE

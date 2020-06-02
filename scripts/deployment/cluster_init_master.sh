ccedcd6d6c51ef9f59962c50eaae249e04a95d52
Oguz Pastirmaci <oguzp@outlook.com>
2020-06-01 19:42:40 -0700

    Update cluster_init_master.sh

--- scripts/deployment/cluster_init_master.sh
+++ scripts/deployment/cluster_init_master.sh
@@ -6,7 +6,7 @@ echo "Cluster postfix: <clusterpostfix>"
 
 . /home/sgeadmin/ocisge/<clusterpostfix>/scripts/info.sh
 
-echo "$(date) Starting cluster initialization"
+echo "$(pdate) Starting cluster initialization"
 # Add ADMIN and SUBMIT host
 MASTER_PRIVATE_IP=$(curl -s http://169.254.169.254/opc/v1/vnics/ | jq -r '.[].privateIp')
 MASTER_HOSTNAME=$(hostname)
@@ -20,19 +20,11 @@ sed -i 's/^SUBMIT_HOST_LIST=.*/SUBMIT_HOST_LIST="'"$MASTER_HOSTNAME"'"/' $CONFIG
 sed -i 's/^CELL_NAME=.*/CELL_NAME="'"$CELL_NAME"'"/' $CONFIG_FILE
 
 cd $SGE_ROOT
-echo "$(date) Adding $MASTER_HOSTNAME as admin and submit host"
+echo "$(pdate) Adding $MASTER_HOSTNAME as admin and submit host"
 ./inst_sge -m -s -auto $CONFIG_FILE
-#sudo cp $SGE_ROOT/$CELL_NAME/common/settings.sh /etc/profile.d/SGE.sh
-#sudo cp $SGE_ROOT/$CELL_NAME/common/settings.csh /etc/profile.d/SGE.csh
-#sudo chmod +x /etc/profile.d/SGE.sh
-#sudo chmod +x /etc/profile.d/SGE.csh
-#. /etc/profile.d/SGE.sh
-#. /etc/profile.d/SGE.csh
 . $SGE_ROOT/$CELL_NAME/common/settings.sh
 
-#echo "$(date) Changing all.q's tmpdir to /nvme/tmp"
-#qconf -rattr queue tmpdir /nvme/tmp all.q
 qconf -Ap $SGE_ROOT/simcores_pe
 qconf -aattr queue pe_list simcores all.q
 
-echo "$(date) Cluster initialization completed"
+echo "$(pdate) Cluster initialization completed"
